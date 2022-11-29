7a7e61acc32621531319af202f96ba5c0f72c590
David Ehley <dehley36@gmail.com>
2022-11-28 18:55:49 -0600

    Adding in the intro content with tailwinds styling

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ function App() {
     <div className="App">
       <Intro />
       <Portfolio />
+      <Timeline />
+      <Contact />
+      <Footer />
     </div>
   )
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,18 @@ import React from "react";
 
 function Intro(){
     return (
-        <div>Intro</div>
+        <div className="flex items-center
+        justify-center flex-col text-center pt-20
+        pb-6">
+            <h1 className="text-4xl md:text-7x1 
+            mb-1 md:mb-3 font-bold">David Ehley</h1>
+            <p className="text-base md:text-x1
+            mb-3 font-medium">Digital Craftsman</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">
+            I am a Software Engineer who enjoys using code to make other peoples lives easier. 
+            When I'm not coding, I enjoy creating videos for my YouTube channel.
+            </p>
+            </div>
     )
 }
 
