d7bf0a880d677dd646c1523d7f66763902904f9c
David Ehley <dehley36@gmail.com>
2022-11-28 19:57:24 -0600

    adding images to portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,18 +1,18 @@
 export default [
     {
         title: 'Developer Portfolio',
-        imgUrl: '',
+        imgUrl: '/assets/port.png',
         stack: ['React', 'Tailwind', 'Vite']
     },
     {
         title: 'Spring Framework',
-        imgUrl: '',
+        imgUrl: '/assets/spring.jpg',
         stack: ['Spring Boot', 'Spring Security', 'Spring Data']
     },
-    {
-        title: 'YouTube',
-        imgUrl: '',
-        stack: ['Pinnacle Studio'],
+    { 
+        title: 'Salesforce',
+        imgUrl: '/assets/salesforce.jpg',
+        stack: ['Apex', 'SOQL', 'Lightning Components'],
         link: 'https://www.youtube.com/channel/UCiJLtZHW8V_CG4iN5oKdFQw'
     }
 ]
