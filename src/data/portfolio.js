e08a82471dc4d8018e7d0b13d77c396522efbf7f
David Ehley <dehley36@gmail.com>
2022-11-28 19:32:46 -0600

    stubbing portfolio items

---
+++ src/data/portfolio.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '',
+        stack: ['React', 'Tailwind', 'Vite']
+    },
+    {
+        title: 'Spring Framework',
+        imgUrl: '',
+        stack: ['Spring Boot', 'Spring Security', 'Spring Data']
+    },
+    {
+        title: 'YouTube',
+        imgUrl: '',
+        stack: ['Pinnacle Studio'],
+        link: 'https://www.youtube.com/channel/UCiJLtZHW8V_CG4iN5oKdFQw'
+    }
+]
