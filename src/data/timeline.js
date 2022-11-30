ca9133a6e5a12160aa128e02f20a73737c56147e
David Ehley <dehley36@gmail.com>
2022-11-29 20:47:41 -0600

    Adding in timeline and dark mode

---
+++ src/data/timeline.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        year: "Today",
+        title: "Senior Software Engineer",
+        duration: "5 years",
+        details: "I work for a Fortune 500 company where I primarily specialize in spring boot micro-services"
+    },
+    {
+        year: "2018",
+        title: "Associate Software Engineer",
+        duration: "1 year",
+        details: "I worked for a Fortune 500 company where I provided 2nd level IT support for 200+ web applications"
+    },
+    {
+        year: "2018",
+        title: "Bachelor's Degree in Computer Science",
+        duration: "4 years",
+        details: "I pursued the knowledge that a computer science degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of mobile development."
+    },
+    {
+        year: "2014",
+        title: "First Line of Code",
+        duration: "the beginning",
+        details: "I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined. My first project was in Visual Basic."
+    }
+]
